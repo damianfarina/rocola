547b72ddecb3afc95acb155a301e89bbd9a93e76
Damián Farina <damianfarina@gmail.com>
2013-01-07 22:48:33 -0300

    Testing grooveshark api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @grooveshark = Rocola::Grooveshark.new
+    @tinysong = Rocola::Tinysong::Search.new params[:search] || {}
+    @tinysong.search
   end
 end
